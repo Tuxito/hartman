ff6c58a722ab13fd0a0a0208d5f9872ee5937954
tuxito <alfredo.leon.fernandez@gmail.com>
2018-02-25 20:46:05 +0100

    Sidebar added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  toggleSideBar(event) {
+    //$('#sidebar').toggleClass('active');
+    
+    console.log(event);
+  }
 }
